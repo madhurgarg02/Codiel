8e26355875d8c31485e89db756962a0d38db32d0
madhurgarg02 <80561819+madhurgarg02@users.noreply.github.com>
2022-01-29 18:18:02 +0530

    adding views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is ready for Codiel!</h1>');
+    return res.render('home',{
+        title: "Home"
+    });
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,5 @@
 module.exports.posts = function(req,res){
-    return res.end('<h1>post!<h1>');
+    return res.render('posts',{
+        title:"Posts"
+    });
 }
