b3090969edc7f69dc61dbbbe95c5c4e764474799
madhurgarg02 <80561819+madhurgarg02@users.noreply.github.com>
2022-02-04 02:41:08 +0530

    folder structure setup complete

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const expressLayout = require('express-ejs-layouts');
+
+app.use(expressLayout);
+
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use(express.static('./assets'));
+
 // use express router
 app.use('/',require('./routes'))
 
+//setting up view engine
 app.set('view engine','ejs');
 app.set('views','./views');
 
